4a8e30b787320a2a9f8e6f79a2f367b7afd6ebf3
Harishma K H <harishmahridas777@gmail.com>
2025-12-23 15:41:26 +0530

    fix:latest updation

--- src/routes/orders.ts
+++ src/routes/orders.ts
@@ -245,7 +245,7 @@ router.get("/dnsorders", authMiddleware, async (req: AuthRequest, res: Response)
 
  
 
-        let orders = await Order.find(query).populate("client", "c_name c_email").exec();
+        let orders = await Order.find(query).populate("client", "c_name c_email  c_company").exec();
         orders = await updateOrderStatuses(orders);
 
         res.status(200).json({ success: true, data: orders });
